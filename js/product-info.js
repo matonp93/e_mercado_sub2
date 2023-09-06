fcfcd695937ee287a0b90574d886071634120dfa
matonp93 <matiasnp12@gmail.com>
2023-09-06 10:20:35 -0300

    realiza la solicitud adecuada para obtener la información de dicho producto y preséntala en product-info.html

--- js/product-info.js
+++ js/product-info.js
@@ -1,5 +1,6 @@
 let localStorageValue = '50921';
 const comentarios = document.getElementsByClassName("comentarios")[0];
+const infoProductos = document.getElementsByClassName("info_prod")[0];
 
 document.addEventListener("DOMContentLoaded",()=>{
    // localStorageValue = localStorage.getItem("cardId");
@@ -33,7 +34,62 @@ document.addEventListener("DOMContentLoaded",()=>{
             comentarios.appendChild(divCard);
         });
   });
+
+  //Pedido a la URL informacion producto 
+  console.log(PRODUCT_INFO_URL + localStorageValue +EXT_TYPE)
+  fetch(PRODUCT_INFO_URL + localStorageValue +EXT_TYPE)
+  .then(response => response.json())
+  .then(data => {
+            //Creación de elementos HTML
+            let divinfoP = document.createElement("div");
+            let divinfoTitle = document.createElement("div");
+            let divinfoImages = document.createElement("div");
+            let h1infoTitle = document.createElement("h1");
+            let pPrice = document.createElement("p");
+            let pDescription = document.createElement("p");
+            let pCategory = document.createElement("p");
+            let pSoldCount = document.createElement("p");
+
+            //array de imagenes
+            let divimgInfo = document.createElement("div");
+            data.images.forEach(image => {
+                let imginfo = document.createElement("img");
+                imginfo.setAttribute("src", image);
+                divimgInfo.classList.add("divImgInfo");
+                divimgInfo.appendChild(imginfo);
+            })
+
+            //Atributos y clases
+            divinfoP.classList.add("divInfoP");
+            divinfoTitle.classList.add("divinfoTitle");
+            divinfoImages.classList.add("divinfoImages");
+            h1infoTitle.classList.add("h1infoTitle");
+            pPrice.classList.add("pDescription");
+            pDescription.classList.add("pDescription");
+            pCategory.classList.add("pDescription");
+            pSoldCount.classList.add("pDescription");
+           
+            //Contenido de cada elemento:
+
+            h1infoTitle.innerHTML = data.name;
+            pPrice.innerHTML = "<b>Precio:</b> <br>" + data.currency + " " + data.cost;
+            pDescription.innerHTML = "<b>Descripción:</b> <br>" + data.description;
+            pCategory.innerHTML = "<b>Categoría:</b> <br>" + data.category;
+            pSoldCount.innerHTML = "<b>Cantidad de Vendidos:</b> <br>" + data.soldCount;
+           
+            //AppendChild's
+            divinfoTitle.appendChild(h1infoTitle);
+            divinfoP.appendChild(pPrice);
+            divinfoP.appendChild(pDescription);
+            divinfoP.appendChild(pCategory);
+            divinfoP.appendChild(pSoldCount);
+            infoProductos.appendChild(divinfoTitle);
+            infoProductos.appendChild(divinfoP);
+            infoProductos.appendChild(divimgInfo);
+  });
 })
+
+
 
 
 
