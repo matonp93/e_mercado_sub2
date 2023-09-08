dafa2230449af185ca283481f22646122b4f30ea
JulianCastill0 <juli.castillo909@gmail.com>
2023-09-07 23:47:34 -0300

    Emprolije el codigo

--- js/product-info.js
+++ js/product-info.js
@@ -4,19 +4,72 @@ const comentariosSection = document.getElementsByClassName('comment-section__com
 const commentList = document.querySelector('.comment-container__comment-list');
 const btnEnviar = document.getElementById('enviar');
 
-document.addEventListener("DOMContentLoaded",()=>{
-localStorageValue = localStorage.getItem("cardId");
-  fetch(PRODUCT_INFO_COMMENTS_URL + localStorageValue +EXT_TYPE)
+document.addEventListener("DOMContentLoaded", () => {
+  let localStorageValue = localStorage.getItem("cardId");
+
+  // FECTH PARA INFO DEL PRODUCTO //
+  fetch(PRODUCT_INFO_URL + localStorageValue + EXT_TYPE)
   .then(response => response.json())
   .then(data => {
-        data.forEach(element => {
-           		// Creación de elementos HTML //
-				let divCard = document.createElement('div');
-				let divCardLoad = document.createElement('div');
-				let divDescription = document.createElement('div');
-				let pTitle = document.createElement('p');
-				let pDescription = document.createElement('p');
-				let image = document.createElement('img');
+    // Creación de elementos HTML //
+    let divinfoP = document.createElement("div");
+    let divinfoTitle = document.createElement("div");
+    let divinfoImages = document.createElement("div");
+    let h1infoTitle = document.createElement("h1");
+    let pPrice = document.createElement("p");
+    let pDescription = document.createElement("p");
+    let pCategory = document.createElement("p");
+    let pSoldCount = document.createElement("p");
+    
+    // Array de imagenes //
+    let divimgInfo = document.createElement("div");
+    data.images.forEach(image => {
+      let imginfo = document.createElement("img");
+      imginfo.setAttribute("src", image);
+      divimgInfo.classList.add("divImgInfo");
+      divimgInfo.appendChild(imginfo);
+    });
+    
+    // Atributos y clases //
+    divinfoP.classList.add("divInfoP");
+    divinfoTitle.classList.add("divinfoTitle");
+    divinfoImages.classList.add("divinfoImages");
+    h1infoTitle.classList.add("h1infoTitle");
+    pPrice.classList.add("pDescription");
+    pDescription.classList.add("pDescription");
+    pCategory.classList.add("pDescription");
+    pSoldCount.classList.add("pDescription");
+               
+    // Contenido de cada elemento //
+    h1infoTitle.innerHTML = data.name;
+    pPrice.innerHTML = "<b>Precio:</b> <br>" + data.currency + " " + data.cost;
+    pDescription.innerHTML = "<b>Descripción:</b> <br>" + data.description;
+    pCategory.innerHTML = "<b>Categoría:</b> <br>" + data.category;
+    pSoldCount.innerHTML = "<b>Cantidad de Vendidos:</b> <br>" + data.soldCount;
+               
+    // AppendChild's //
+    divinfoTitle.appendChild(h1infoTitle);
+    divinfoP.appendChild(pPrice);
+    divinfoP.appendChild(pDescription);
+    divinfoP.appendChild(pCategory);
+    divinfoP.appendChild(pSoldCount);
+    infoProductos.appendChild(divinfoTitle);
+    infoProductos.appendChild(divinfoP);
+    infoProductos.appendChild(divimgInfo);
+  });
+
+  // FETCH PARA COMENTARIOS DEL PRODUCTO //
+  fetch(PRODUCT_INFO_COMMENTS_URL + localStorageValue + EXT_TYPE)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(element => {
+        // Creación de elementos HTML //
+        let divCard = document.createElement('div');
+        let divCardLoad = document.createElement('div');
+        let divDescription = document.createElement('div');
+        let pTitle = document.createElement('p');
+        let pDescription = document.createElement('p');
+        let image = document.createElement('img');
 
         // Estrellas en los comentarios //
         let estrellas = [];
@@ -32,78 +85,29 @@ localStorageValue = localStorage.getItem("cardId");
           estrellas[i].style.color = "#fd4";
         };
 
-				// Atributos y clases //
-				image.src = '/img/gitlab.svg';
-				pTitle.classList.add('comment-title');
-				divCard.classList.add('cards');
-				divCardLoad.classList.add('tarjeta_load');
-				divDescription.classList.add('tarjeta_load_extreme_description');
+        // Atributos y clases //
+        image.src = '/img/gitlab.svg';
+        pTitle.classList.add('comment-title');
+        divCard.classList.add('cards');
+        divCardLoad.classList.add('tarjeta_load');
+        divDescription.classList.add('tarjeta_load_extreme_description');
         pTitle.innerHTML = element.user + ' ' + pTitle.innerHTML;
-				pDescription.innerHTML = element.description;
-				// AppendChild's //
-				divDescription.appendChild(pTitle);
-				divDescription.appendChild(pDescription);
-				divCardLoad.appendChild(image);
-				divCard.appendChild(divCardLoad);
-				divCard.appendChild(divDescription);
-				commentList.appendChild(divCard);   
-          })
+        pDescription.innerHTML = element.description;
 
-        });
-        console.log(PRODUCT_INFO_URL + localStorageValue +EXT_TYPE)
-        fetch(PRODUCT_INFO_URL + localStorageValue +EXT_TYPE)
-        .then(response => response.json())
-        .then(data => {
-                  //Creación de elementos HTML
-                  let divinfoP = document.createElement("div");
-                  let divinfoTitle = document.createElement("div");
-                  let divinfoImages = document.createElement("div");
-                  let h1infoTitle = document.createElement("h1");
-                  let pPrice = document.createElement("p");
-                  let pDescription = document.createElement("p");
-                  let pCategory = document.createElement("p");
-                  let pSoldCount = document.createElement("p");
-      
-                  //array de imagenes
-                  let divimgInfo = document.createElement("div");
-                  data.images.forEach(image => {
-                      let imginfo = document.createElement("img");
-                      imginfo.setAttribute("src", image);
-                      divimgInfo.classList.add("divImgInfo");
-                      divimgInfo.appendChild(imginfo);
-                  })
-      
-                  //Atributos y clases
-                  divinfoP.classList.add("divInfoP");
-                  divinfoTitle.classList.add("divinfoTitle");
-                  divinfoImages.classList.add("divinfoImages");
-                  h1infoTitle.classList.add("h1infoTitle");
-                  pPrice.classList.add("pDescription");
-                  pDescription.classList.add("pDescription");
-                  pCategory.classList.add("pDescription");
-                  pSoldCount.classList.add("pDescription");
-                 
-                  //Contenido de cada elemento:
-      
-                  h1infoTitle.innerHTML = data.name;
-                  pPrice.innerHTML = "<b>Precio:</b> <br>" + data.currency + " " + data.cost;
-                  pDescription.innerHTML = "<b>Descripción:</b> <br>" + data.description;
-                  pCategory.innerHTML = "<b>Categoría:</b> <br>" + data.category;
-                  pSoldCount.innerHTML = "<b>Cantidad de Vendidos:</b> <br>" + data.soldCount;
-                 
-                  //AppendChild's
-                  divinfoTitle.appendChild(h1infoTitle);
-                  divinfoP.appendChild(pPrice);
-                  divinfoP.appendChild(pDescription);
-                  divinfoP.appendChild(pCategory);
-                  divinfoP.appendChild(pSoldCount);
-                  infoProductos.appendChild(divinfoTitle);
-                  infoProductos.appendChild(divinfoP);
-                  infoProductos.appendChild(divimgInfo);
-        });
-        btnEnviar.addEventListener('click', () => {
-            agregarComentario();
-        });
+        // AppendChild's //
+        divDescription.appendChild(pTitle);
+        divDescription.appendChild(pDescription);
+        divCardLoad.appendChild(image);
+        divCard.appendChild(divCardLoad);
+        divCard.appendChild(divDescription);
+        commentList.appendChild(divCard);   
+      });
+    });
+
+    btnEnviar.addEventListener('click', () => {
+      agregarComentario();
+    });
+
   });
 
 // AGREGAR COMENTARIOS //
