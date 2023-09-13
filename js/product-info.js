68d6d25d25c187e7ef114065250763f1580c93dd
Jonathan Hernández <jonathanhernandezgonzatto@gmail.com>
2023-09-13 11:15:40 -0300

    mas colores en los comentarios

--- js/product-info.js
+++ js/product-info.js
@@ -1,134 +1,132 @@
-const comentarios = document.getElementsByClassName("comentarios")[0];
-const infoProductos = document.getElementsByClassName("info_prod")[0];
+const comentarios = document.getElementsByClassName('comentarios')[0];
+const infoProductos = document.getElementsByClassName('info_prod')[0];
 const comentariosSection = document.getElementsByClassName('comment-section__comment-container')[0];
 const commentList = document.querySelector('.comment-container__comment-list');
 const btnEnviar = document.getElementById('enviar');
 
-document.addEventListener("DOMContentLoaded", () => {
-  let localStorageValue = localStorage.getItem("cardId");
+document.addEventListener('DOMContentLoaded', () => {
+	let localStorageValue = localStorage.getItem('cardId');
 
-  // FECTH PARA INFO DEL PRODUCTO //
-  fetch(PRODUCT_INFO_URL + localStorageValue + EXT_TYPE)
-  .then(response => response.json())
-  .then(data => {
-    // Creación de elementos HTML //
-    let divinfoTitle = document.createElement("div");
-    let divinfoImages = document.createElement("div");
-    let divPrice = document.createElement("div");
-    let divCategory = document.createElement("div");
-    let divSoldCount = document.createElement("div");
-    let divinfoP = document.createElement("div");
-    let h1infoTitle = document.createElement("h1");
-    let pPrice = document.createElement("p");
-    let pDescription = document.createElement("p");
-    let pCategory = document.createElement("p");
-    let pSoldCount = document.createElement("p");
-    
-    // Array de imagenes //
-    let divimgInfo = document.createElement("div");
-    data.images.forEach(image => {
-      let imginfo = document.createElement("img");
-      imginfo.setAttribute("src", image);
-      divimgInfo.classList.add("divImgInfo");
-      divimgInfo.appendChild(imginfo);
-    });
-    
-    // Atributos y clases //
-    divinfoP.classList.add("divInfoP");
-    divinfoTitle.classList.add("divinfoTitle");
-    divinfoImages.classList.add("divinfoImages");
-    divPrice.classList.add("divPrice");
-    divCategory.classList.add("divCategory");
-    divSoldCount.classList.add("divSoldCount");
-    h1infoTitle.classList.add("h1infoTitle");
-    pPrice.classList.add("pPrice");
-    pDescription.classList.add("pDescription");
-    pCategory.classList.add("pCategory");
-    pSoldCount.classList.add("pSoldCount");
-               
-    // Contenido de cada elemento //
-    h1infoTitle.innerHTML = data.name;
-    pPrice.innerHTML = data.currency + " " + data.cost;
-    pDescription.innerHTML = "<b>Descripción:</b> <br>" + data.description;
-    pCategory.innerHTML = " ";
-    pSoldCount.innerHTML = "<b>Vendidos:</b>   " + data.soldCount;
-               
-    // AppendChild's //
-    divinfoTitle.appendChild(h1infoTitle);
-    divPrice.appendChild(pPrice);
-    divinfoP.appendChild(pDescription);
-    divCategory.appendChild(pCategory);
-    divSoldCount.appendChild(pSoldCount);
-    infoProductos.appendChild(divinfoTitle);
-    infoProductos.appendChild(divCategory);
-    infoProductos.appendChild(divPrice);
-    infoProductos.appendChild(divSoldCount);
-    infoProductos.appendChild(divimgInfo);
-    infoProductos.appendChild(divinfoP);
-  });
+	// FECTH PARA INFO DEL PRODUCTO //
+	fetch(PRODUCT_INFO_URL + localStorageValue + EXT_TYPE)
+		.then((response) => response.json())
+		.then((data) => {
+			// Creación de elementos HTML //
+			let divinfoTitle = document.createElement('div');
+			let divinfoImages = document.createElement('div');
+			let divPrice = document.createElement('div');
+			let divCategory = document.createElement('div');
+			let divSoldCount = document.createElement('div');
+			let divinfoP = document.createElement('div');
+			let h1infoTitle = document.createElement('h1');
+			let pPrice = document.createElement('p');
+			let pDescription = document.createElement('p');
+			let pCategory = document.createElement('p');
+			let pSoldCount = document.createElement('p');
 
-  // FETCH PARA COMENTARIOS DEL PRODUCTO //
-  fetch(PRODUCT_INFO_COMMENTS_URL + localStorageValue + EXT_TYPE)
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(element => {
-        // Creación de elementos HTML //
-        let divCard = document.createElement('div');
-        let divCardLoad = document.createElement('div');
-        let divDescription = document.createElement('div');
-        let pTitle = document.createElement('p');
-        let pDescription = document.createElement('p');
-        let image = document.createElement('object');
+			// Array de imagenes //
+			let divimgInfo = document.createElement('div');
+			data.images.forEach((image) => {
+				let imginfo = document.createElement('img');
+				imginfo.setAttribute('src', image);
+				divimgInfo.classList.add('divImgInfo');
+				divimgInfo.appendChild(imginfo);
+			});
 
-        // Estrellas en los comentarios //
-        let estrellas = [];
-        for (let i = 0; i < 5; i++){
-          estrellas.push(document.createElement("label"));
-          estrellas[i].classList.add("fa");
-          estrellas[i].classList.add("fa-star");
-          estrellas[i].style.color = "grey";
-          pTitle.appendChild(estrellas[i]);
-        };
+			// Atributos y clases //
+			divinfoP.classList.add('divInfoP');
+			divinfoTitle.classList.add('divinfoTitle');
+			divinfoImages.classList.add('divinfoImages');
+			divPrice.classList.add('divPrice');
+			divCategory.classList.add('divCategory');
+			divSoldCount.classList.add('divSoldCount');
+			h1infoTitle.classList.add('h1infoTitle');
+			pPrice.classList.add('pPrice');
+			pDescription.classList.add('pDescription');
+			pCategory.classList.add('pCategory');
+			pSoldCount.classList.add('pSoldCount');
 
-        for (let i = 0; i < element.score; i++){
-          estrellas[i].style.color = "#fd4";
-        };
+			// Contenido de cada elemento //
+			h1infoTitle.innerHTML = data.name;
+			pPrice.innerHTML = data.currency + ' ' + data.cost;
+			pDescription.innerHTML = '<b>Descripción:</b> <br>' + data.description;
+			pCategory.innerHTML = ' ';
+			pSoldCount.innerHTML = '<b>Vendidos:</b>   ' + data.soldCount;
 
-        // Atributos y clases //
-        image.data = 'gitlab.svg';
-        image.type = "image/svg+xml";
-        pTitle.classList.add('comment-title');
-        divCard.classList.add('cards');
-        divCardLoad.classList.add('tarjeta_load');
-        divDescription.classList.add('tarjeta_load_extreme_description');
-        pTitle.innerHTML = element.user + ' ' + pTitle.innerHTML;
-        pDescription.innerHTML = element.description;
+			// AppendChild's //
+			divinfoTitle.appendChild(h1infoTitle);
+			divPrice.appendChild(pPrice);
+			divinfoP.appendChild(pDescription);
+			divCategory.appendChild(pCategory);
+			divSoldCount.appendChild(pSoldCount);
+			infoProductos.appendChild(divinfoTitle);
+			infoProductos.appendChild(divCategory);
+			infoProductos.appendChild(divPrice);
+			infoProductos.appendChild(divSoldCount);
+			infoProductos.appendChild(divimgInfo);
+			infoProductos.appendChild(divinfoP);
+		});
 
-        // AppendChild's //
-        divDescription.appendChild(pTitle);
-        divDescription.appendChild(pDescription);
-        divCardLoad.appendChild(image);
-        divCard.appendChild(divCardLoad);
-        divCard.appendChild(divDescription);
-        commentList.appendChild(divCard);  
-      });
-    });
-    btnEnviar.addEventListener('click', () => {
-      agregarComentario();
-    });
+	// FETCH PARA COMENTARIOS DEL PRODUCTO //
+	fetch(PRODUCT_INFO_COMMENTS_URL + localStorageValue + EXT_TYPE)
+		.then((response) => response.json())
+		.then((data) => {
+			data.forEach((element) => {
+				// Creación de elementos HTML //
+				let divCard = document.createElement('div');
+				let divCardLoad = document.createElement('div');
+				let divDescription = document.createElement('div');
+				let pTitle = document.createElement('p');
+				let pDescription = document.createElement('p');
+				let image = document.createElement('object');
 
-  });
+				// Estrellas en los comentarios //
+				let estrellas = [];
+				for (let i = 0; i < 5; i++) {
+					estrellas.push(document.createElement('label'));
+					estrellas[i].classList.add('fa');
+					estrellas[i].classList.add('fa-star');
+					estrellas[i].style.color = 'grey';
+					pTitle.appendChild(estrellas[i]);
+				}
+
+				for (let i = 0; i < element.score; i++) {
+					estrellas[i].style.color = '#fd4';
+				}
+
+				// Atributos y clases //
+				image.data = 'gitlab.svg';
+				image.type = 'image/svg+xml';
+				pTitle.classList.add('comment-title');
+				divCard.classList.add('cards');
+				divCardLoad.classList.add('tarjeta_load');
+				divDescription.classList.add('tarjeta_load_extreme_description');
+				pTitle.innerHTML = element.user + ' ' + pTitle.innerHTML;
+				pDescription.innerHTML = element.description;
+
+				// AppendChild's //
+				divDescription.appendChild(pTitle);
+				divDescription.appendChild(pDescription);
+				divCardLoad.appendChild(image);
+				divCard.appendChild(divCardLoad);
+				divCard.appendChild(divDescription);
+				commentList.appendChild(divCard);
+			});
+		});
+	btnEnviar.addEventListener('click', () => {
+		agregarComentario();
+	});
+});
 
 // AGREGAR COMENTARIOS //
-function agregarComentario(){
-
-  // Creacion de elementos HTML //
-  let divCard = document.createElement('div');
-  let divCardLoad = document.createElement('div');
-  let divDescription = document.createElement('div');
-  let pTitle = document.createElement('p');
-  let pDescription = document.createElement('p');
-  let image = document.createElement('object');
+function agregarComentario() {
+	// Creacion de elementos HTML //
+	let divCard = document.createElement('div');
+	let divCardLoad = document.createElement('div');
+	let divDescription = document.createElement('div');
+	let pTitle = document.createElement('p');
+	let pDescription = document.createElement('p');
+	let image = document.createElement('object');
 
 	// Nombre del user //
 	let user = localStorage.getItem('email');
@@ -147,55 +145,55 @@ function agregarComentario(){
 		}
 	});
 
-  // Agregar estrellas al comentario //
-  let estrellas = [];
-  for (let i = 0; i < 5; i++){
-    estrellas.push(document.createElement("label"));
-    estrellas[i].classList.add("fa");
-    estrellas[i].classList.add("fa-star");
-    estrellas[i].style.color = "grey";
-    pTitle.appendChild(estrellas[i]);
-  };
+	// Agregar estrellas al comentario //
+	let estrellas = [];
+	for (let i = 0; i < 5; i++) {
+		estrellas.push(document.createElement('label'));
+		estrellas[i].classList.add('fa');
+		estrellas[i].classList.add('fa-star');
+		estrellas[i].style.color = 'grey';
+		pTitle.appendChild(estrellas[i]);
+	}
 
-  for (let i = 0; i < puntaje; i++){
-    estrellas[i].style.color = "#fd4";
-  };
+	for (let i = 0; i < puntaje; i++) {
+		estrellas[i].style.color = '#fd4';
+	}
 
-  // Atributos y clases //
-  image.data = 'gitlab.svg';
-  image.type = "image/svg+xml";
-  pTitle.classList.add('comment-title');
-  divCard.classList.add('cards');
-  divCardLoad.classList.add('tarjeta_load');
-  divDescription.classList.add('tarjeta_load_extreme_description');
-  pTitle.innerHTML = nombreUser + ' ' + pTitle.innerHTML;
-  pDescription.innerHTML = comentario;
+	// Atributos y clases //
+	image.data = 'gitlab.svg';
+	image.type = 'image/svg+xml';
+	pTitle.classList.add('comment-title');
+	divCard.classList.add('cards');
+	divCardLoad.classList.add('tarjeta_load');
+	divDescription.classList.add('tarjeta_load_extreme_description');
+	pTitle.innerHTML = nombreUser + ' ' + pTitle.innerHTML;
+	pDescription.innerHTML = comentario;
 
-  // AppendChild's //
-  divDescription.appendChild(pTitle);
-  divDescription.appendChild(pDescription);
-  divCardLoad.appendChild(image);
-  divCard.appendChild(divCardLoad);
-  divCard.appendChild(divDescription);
-  commentList.appendChild(divCard);
+	// AppendChild's //
+	divDescription.appendChild(pTitle);
+	divDescription.appendChild(pDescription);
+	divCardLoad.appendChild(image);
+	divCard.appendChild(divCardLoad);
+	divCard.appendChild(divDescription);
+	commentList.appendChild(divCard);
 
-  // Borrar inputs //
-  document.getElementById("add-comment__input").value = "";
-  puntajes.forEach(element => element.checked = false);
-  ResetearColores();
-};
+	// Borrar inputs //
+	document.getElementById('add-comment__input').value = '';
+	puntajes.forEach((element) => (element.checked = false));
+	ResetearColores();
+}
 
 window.onload = ResetearColores;
-function ResetearColores(){
-  let colores = ["blue","pink","orange","violet"];
-  let contador = 0;
-  setTimeout(() => {
-    Array.from(document.getElementsByTagName("object")).forEach(element =>{
-      if (contador > colores.length-1){
-        contador = 0
-      }
-        element.contentDocument.getElementsByTagName("svg")[0].style.color = colores[contador];
-        contador++
-    })
-  }, 200);
+function ResetearColores() {
+	let colores = ['#a12957', '#3a3aa8', '#59a129', 'orange', 'violet', '#a12929', '#29a18d'];
+	let contador = 0;
+	setTimeout(() => {
+		Array.from(document.getElementsByTagName('object')).forEach((element) => {
+			if (contador > colores.length - 1) {
+				contador = 0;
+			}
+			element.contentDocument.getElementsByTagName('svg')[0].style.color = colores[contador];
+			contador++;
+		});
+	}, 200);
 }
