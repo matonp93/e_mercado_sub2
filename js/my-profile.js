56130fc38ed90be521c398b49cd32f8eb8d192a5
Bruno Rodriguez <bruno.rodriguez2@estudiante.ceibal.edu.uy>
2023-11-03 21:30:05 -0300

    lógica my-profile y bugfix de cart

--- js/my-profile.js
+++ js/my-profile.js
@@ -9,16 +9,20 @@ imagenSelector = document.getElementById("imagenSelector"),
 imagenPlaceholder = document.getElementById("imagenPlaceholder"),
 profileForm = document.getElementById("profileForm"),
 profileFormButton = document.getElementById("profileFormButton");
-let image,imageText, usersArray = new Array(), reader = new FileReader();
+let image, imageText, usersArray = new Array(), reader = new FileReader();
 
 document.addEventListener("DOMContentLoaded",()=>{
     if(localStorage.users){
         usersArray = JSON.parse(localStorage.users);
+        getUserInfo();
     }
+    email.value = localStorage.email;
+
     imagenSelector.addEventListener("change",()=>{
        image = imagenSelector.files[0];
        reader.readAsDataURL(image);
     })
+    
     reader.addEventListener("load",()=>{
         imageText = reader.result;
         imagenPlaceholder.src = imageText;
@@ -26,7 +30,14 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     profileForm.addEventListener("submit",(event)=>{
         event.preventDefault();
-        let user = new Object();
+        let user;
+        let existingUser = usersArray.find(element => element.email == localStorage.email);
+        if(existingUser){
+            user = existingUser;
+        }else{
+            user = new Object();
+            usersArray.push(user);
+        }
         user.nombre = nombre.value;
         user.segundoNombre = segundoNombre.value;
         user.apellido = apellido.value;
@@ -34,33 +45,23 @@ document.addEventListener("DOMContentLoaded",()=>{
         user.usuario = usuario.value;
         user.email = email.value;
         user.telefono = telefono.value;
-        user.image = imageText;
-        usersArray.push(user);
+        if(imageText){
+            user.image = imageText;
+        }
         localStorage.users = JSON.stringify(usersArray);
     })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
+function getUserInfo(){
+    let user = usersArray.find(element => element.email == localStorage.email);
+    if(user){
+        nombre.value = user.nombre;
+        segundoNombre.value = user.segundoNombre;
+        apellido.value = user.apellido;
+        segundoApellido.value = user.segundoApellido;
+        usuario.value = user.usuario;
+        email.value = user.email;
+        telefono.value = user.telefono;
+        imagenPlaceholder.src = user.image;
+    }
+}
